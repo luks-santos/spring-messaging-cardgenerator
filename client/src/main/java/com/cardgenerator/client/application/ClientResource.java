f7e305f1f08adb64322014c7b0b0ecc27fd1c12c
luks-santos <57961643+luks-santos@users.noreply.github.com>
2025-05-13 21:17:30 -0300

    Add gateway service with routing and health check endpoint

--- client/src/main/java/com/cardgenerator/client/application/ClientResource.java
+++ client/src/main/java/com/cardgenerator/client/application/ClientResource.java
@@ -3,6 +3,7 @@ package com.cardgenerator.client.application;
 import com.cardgenerator.client.application.representation.ClientSaveRequest;
 import com.cardgenerator.client.domain.Client;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +18,16 @@ import java.net.URI;
 @RestController
 @RequestMapping("clients")
 @RequiredArgsConstructor
+@Slf4j
 public class ClientResource {
 
     private final ClientService service;
+
+    @GetMapping
+    public String healthCheck() {
+        log.info("Health check");
+        return "ok";
+    }
 
     @PostMapping
     public ResponseEntity<URI> save(@RequestBody ClientSaveRequest request) {
